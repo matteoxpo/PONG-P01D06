598dac942814c03f47b883736986518b2314a329
Kwaltz Buck <kwaltzbu@se-b4.nsk.21-school.ru>
2022-07-30 12:46:47 +0700

    start

--- src/pong.c
+++ src/pong.c
@@ -1,5 +1,47 @@
-//I WANT TO PLAY WITH YOU
-//        YOUR FRIEND, AI
+#include <stdio.h>
+
+#define W 80
+#define H 25
+
+void game_pong() {
+    int lScore = 0;
+    int rScore = 0;
+    char input = 0;
+    do {
+        printf("Left Score:%d", lScore);
+        for (int i = 0; i < 29; i++) {
+            printf(" ");
+        }
+        printf("Right Score:%d\n", rScore);
+        for (int i = 0; i < H; i++) {
+            for (int j = 0; j < W; j++) {
+                if (i == 0  || i == H - 1) {
+                    printf("#");
+                } else {
+                    if (j == 0 || j == W - 1){
+                        printf("#");
+                    } else {
+                        if (j == 39 || j == 40){
+                            printf("|");
+                        } else {
+                            printf(" ");
+                        }
+                    }
+                }
+            }
+            printf("\n");
+        }
+        printf("\nA/Z, K/M to play game\nenter e to exit\n");
+        scanf("%c",&input);
+        printf("\n!%c!",input);
+    } while(input != 'e' || lScore != 21 || rScore != 21);
+}
 
 
 
+
+
+int main() {
+    game_pong();
+    return 0;
+}
