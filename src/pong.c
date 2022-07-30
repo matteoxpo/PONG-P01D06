63ca790f874e4d816e282b80abc2443ea7fa94b2
Kwaltz Buck <kwaltzbu@pr-a6.nsk.21-school.ru>
2022-07-30 13:53:15 +0700

    some changes

--- src/pong.c
+++ src/pong.c
@@ -6,24 +6,27 @@
 void game_pong() {
     int lScore = 0;
     int rScore = 0;
-    char input = 0;
+    char input;
     do {
         printf("Left Score:%d", lScore);
         for (int i = 0; i < 29; i++) {
             printf(" ");
         }
         printf("Right Score:%d\n", rScore);
-        for (int i = 0; i < H; i++) {
-            for (int j = 0; j < W; j++) {
-                if (i == 0  || i == H - 1) {
-                    printf("#");
-                } else {
-                    if (j == 0 || j == W - 1){
-                        printf("#");
+        for (int i = 0; i < H + 2; i++) { // i - строки
+            
+            for (int j = 0; j < W + 2; j++) { // j - столбцы
+                
+                if (i == 0  || i == H + 1) { // СЛУЧАЙ НИЖНЕЙ И ВЕРХНЕЙ ГРАНИЦЫ ПОЛЯ
+                    printf("#");             // выводим на всю строку #
+                } else { //
+                    if (j == 0 || j == W + 1){ // СЛУЧАЙ ЛЕВОЙ И ПРАВОЙ ГРАНИЦЫ ПОЛЯ
+                        printf("#");           // выводим символ #
                     } else {
-                        if (j == 39 || j == 40){
+                        if (j == 40 || j == 41 ) {// Вывод сетки (добавить условие что шарик не на той же координате)
                             printf("|");
                         } else {
+                            //
                             printf(" ");
                         }
                     }
@@ -31,10 +34,10 @@ void game_pong() {
             }
             printf("\n");
         }
-        printf("\nA/Z, K/M to play game\nenter e to exit\n");
+        printf("\nA/Z, K/M to play game\nenter e to exit\nInput:");
         scanf("%c",&input);
-        printf("\n!%c!",input);
-    } while(input != 'e' || lScore != 21 || rScore != 21);
+        printf("\n!%c!",input);//|| lScore != 21 || rScore != 21
+    } while(input != 'e' );
 }
 
 
@@ -42,6 +45,11 @@ void game_pong() {
 
 
 int main() {
+    
+    // for while
+    //int i = 0;
+    //
+    
     game_pong();
     return 0;
 }
