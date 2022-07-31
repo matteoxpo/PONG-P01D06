3c05789b739d4c2a433d75a2d031b33c91fd170c
Kwaltz Buck <kwaltzbu@cl-b7.nsk.21-school.ru>
2022-07-31 13:35:15 +0700

    few changes

--- src/pong.c
+++ src/pong.c
@@ -24,7 +24,7 @@ void game_pong(){
 
     
     int BallXC = W/2;
-    int BallYC = H/2;
+    int BallYC = H/2 + 3;
     int BallXS = -1;
     int BallYS = 1;
 
@@ -35,8 +35,8 @@ void game_pong(){
     int rPadYC  = H/2;
 
     int flag = 1;
-    printf("\nTO PLAY GAME:\npush A K - to up left/right paddle\npush Z M - to down left/right paddle\nAny symbol to start:");
-    getchar();
+    printf("\nTO PLAY GAME:\npush A K - to up left/right paddle\npush Z M - to down left/right paddle\n");
+    //getchar();
     do {
         fpurge(stdin);
         printf("Left Score:%d", lScore);
@@ -83,42 +83,51 @@ void game_pong(){
             }
         }
 
-        if (BallYC + BallYS == 0 || BallYC + BallYS == H + 1)
-            BallYS*=-1;
-        
-
-        if (BallXC == 0) {
+        if (BallXC + BallXS == 0) {
             rScore++;
             BallXC = W/2;
             BallYC = H/2;
+            continue;
         }
-        if (BallXC == W + 1) {
+        if (BallXC + BallXS == W + 1) {
             lScore++;
             BallXC = W/2;
             BallYC = H/2;
+            continue;
         }
 
         if (lScore == 21 || rScore == 21)
             break;
-        
+
+        if ((BallXC + BallXS == lPadXC) || (BallXC + BallXS == rPadXC)) {
+            if (BallXC + BallXS == lPadXC) {
+                if (BallYC == lPadYC || BallYC == lPadYC - 1 || BallYC == lPadYC + 1) {
+                    BallXS *= -1;
+                    BallYS *= -1;
+                }
+            } 
+
+            if (BallXC + BallXS == rPadXC) {
+                if (BallYC == rPadYC || BallYC == rPadYC - 1 || BallYC == rPadYC + 1) {
+                    BallXS *= -1;
+                    BallYS *= -1;
+                }
+            } 
+        } else {
+            if (BallYC + BallYS == 0 || BallYC + BallYS == H + 1) {
+                BallYS *= -1;
+            }
+        }
+
+
+        printf("\n    BallXS:%d, BallYS:%d, BallXC:%d, BallYC:%d \n", BallXS, BallYS, BallXC, BallYC);
+
         BallXC+=BallXS;
         BallYC+=BallYS;
-
-        if (BallXC == lPadXC && (BallYC == lPadYC || BallYC == lPadYC +1 || BallYC == lPadYC - 1)){
-            BallYS*=-1;
-            BallXS*=-1;
-        }
-
-        if (BallXC == rPadXC && (BallYC == rPadYC || BallYC == rPadYC + 1 || BallYC == rPadYC - 1)){
-            BallYS*=-1;
-            BallXS*=-1;
-            printf("CHANGED!");
-        }
-
         //scanf("%c",&input);
-        input = getchar();
-        switch(input){
-            case 'a':
+        input = getchar(); // ввод команды с консоли
+        switch(input){ // почитать как работает switch case 
+            case 'a':  
                 if(lPadYC - 2 != 0) 
                     lPadYC--;    
                 flag = 1;
